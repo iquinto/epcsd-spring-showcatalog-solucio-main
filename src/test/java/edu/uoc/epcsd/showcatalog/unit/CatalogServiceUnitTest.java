875ac8c4a5c34c615af8b6a824687f03ce3d53d5
iquinto <broganzquinto@gmail.com>
2022-05-21 18:11:53 +0200

    initial commit

---
+++ src/test/java/edu/uoc/epcsd/showcatalog/unit/CatalogServiceUnitTest.java
@@ -0,0 +1,54 @@
+package edu.uoc.epcsd.showcatalog.unit;
+
+import edu.uoc.epcsd.showcatalog.domain.Category;
+import edu.uoc.epcsd.showcatalog.domain.Performance;
+import edu.uoc.epcsd.showcatalog.domain.Show;
+import edu.uoc.epcsd.showcatalog.domain.Status;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CatalogServiceUnitTest {
+
+    @Test
+    public void createAndCancelShowTest() {
+
+        //given
+        Category category = new Category(1L, "music", "Music Category");
+        Performance performance = new Performance(LocalDate.of(2022,6,16), LocalTime.of(2,30), "www.youtube.com", 56L, Status.CREATED);
+
+        //when
+        Set<Performance> performances = new HashSet<>();
+        performances.add(performance);
+        Show show = new Show();
+        show.setId(1L);
+        show.setCategory(category);
+        show.setName("U2 Tour");
+        show.setDescription("This is U2");
+        show.setImage("u2.jpeg");
+        show.setPrice(150.0);
+        show.setDuration(190.0);
+        show.setOnSaleDate(LocalDate.of(2022,6,16));
+        show.setPerformances(performances);
+
+        //then
+        assertThat(show).isNotNull();
+        assertThat(show.getCategory()).isEqualTo(category);
+        assertThat(show.getStatus()).isEqualTo(Status.CREATED);
+
+        //when
+        show.cancel();
+
+        //then:
+        assertThat(show.getStatus()).isEqualTo(Status.CANCELLED);
+        assertThat(performance.getStatus()).isEqualTo(Status.CANCELLED);
+
+
+    }
+
+}
